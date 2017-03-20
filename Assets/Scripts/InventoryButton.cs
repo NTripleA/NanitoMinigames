1e4a001197df3ad3d567996eb41952e1f1548a2c
Nelson Alemar <Nelson Alemar>
2017-03-20 01:24:16 -0400

    Aqui esta el shelf terminado. Pushing to checkout

---
+++ Assets/Scripts/InventoryButton.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InventoryButton : MonoBehaviour {
+
+    public Inventory inv;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void BeenClicked()
+    {
+        Debug.Log("AQUIASDA");
+        inv.ReturnFromWhenceYouCame();
+    }
+}
---
+++ Assets/Scripts/ItemData.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+[Serializable]
+public class ItemData : MonoBehaviour {
+
+    public bool picked = false;
+    public Inventory inventory;
+    public Sprite obj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+    public ItemData Clone()
+    {
+        ItemData clone = new ItemData();
+        clone.tag = this.tag;
+        clone.transform.position = this.transform.position;
+        clone.transform.localScale = this.transform.localScale;
+
+        return clone;
+    }
+
+    void Update()
+    {
+      
+    }
+    void OnMouseDown()
+    {
+        if (!inventory.isUsed()) //so we won't have more than one item being picked
+        {
+            this.gameObject.SetActive(false);
+            picked = true;
+            inventory.changeSprite(obj, this);
+        }
+    }
+
+
+}
