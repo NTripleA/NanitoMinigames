7288e50d8ed109b3c19324dd32f04e137ad2d7ff
Nelson Alemar <Nelson Alemar>
2017-03-22 12:13:56 -0400

    Doing something in this branch

---
+++ Assets/Scripts/CameraSwitcher.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitcher : MonoBehaviour {
+
+    private bool isMain;
+    private bool isDesk;
+    private bool isShelf;
+
+    public GameObject mainCam;
+    public GameObject deskCam;
+    public GameObject shelfCam;
+
+	// Use this for initialization
+	void Start () {
+
+        isMain  =  true;
+        isDesk  = false;
+        isShelf = false;
+
+        mainCam.SetActive(true);
+        deskCam.SetActive(false);
+        deskCam.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            isDesk  = false;
+            isMain  = false;
+            isShelf =  true;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            isDesk  =  true;
+            isMain  = false;
+            isShelf = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            isDesk  = false;
+            isMain  =  true;
+            isShelf = false;
+        }
+
+        if (isMain)
+        {
+            mainCam.SetActive(true);
+            deskCam.SetActive(false);
+            shelfCam.SetActive(false);
+        }
+
+        if (isDesk)
+        {
+          //  mainCam.SetActive(false);
+            deskCam.SetActive(true);
+            shelfCam.SetActive(false);
+        }
+
+        if (isShelf)
+        {
+          //  mainCam.SetActive(false);
+            deskCam.SetActive(false);
+            shelfCam.SetActive(true);
+        }
+    }
+
+    public void setDeskCam()
+    {
+        isDesk  =  true;
+        isMain  = false;
+        isShelf = false;
+    }
+
+    public void setShelfCam()
+    {
+        isDesk  = false;
+        isMain  = false;
+        isShelf =  true;
+    }
+
+    public void setMainCam()
+    {
+        isDesk  = false;
+        isMain  =  true;
+        isShelf = false;
+    }
+
+    public bool getMainCam()
+    {
+        return isMain;
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public bool isPaused;
+
+    public GameObject PauseMenuCanvas;
+    public GameObject image;
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            PauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            PauseMenuCanvas.SetActive(false);
+            Time.timeScale = 1f;
+        }
+
+
+	}
+
+    public void setPaused(bool b)
+    {
+        isPaused = b;
+    }
+
+    public bool getIsPaused()
+    {
+        return isPaused;
+    }
+
+    public void Resume()
+    {
+        isPaused = false;
+        image.SetActive(false);
+    }
+
+    public void Audio()
+    {
+        image.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
