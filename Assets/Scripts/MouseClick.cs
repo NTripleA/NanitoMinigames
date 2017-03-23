f8552041ba118c6f0ba21a91ebc351d70af620a0
JEduardoPerez <eduardo.perez11@upr.edu>
2017-03-23 00:40:05 -0400

    Assets

---
+++ Assets/Scripts/MouseClick.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour {
+
+    public PlayerController player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if(gameObject.name == "Walls")
+        {
+            player.SendMessage("setNoTouch");
+        }
+        else if(gameObject.name == "Shelf Tech")
+        {
+            player.SendMessage("setShelf");
+        }
+        else if(gameObject.name == "Multiple Monitor PC")
+        {
+            player.SendMessage("setDesk");
+        }
+        else if(gameObject.name == "SettingsButton")
+        {
+            player.SendMessage("setSettings");
+        }
+    }
+}
